1349808c18ed150a8b147be8ad736453f615f373
Willem <willem.verdoodt@gmail.com>
2022-11-24 10:26:25 +0100

    dependencies

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ require("dotenv").config(); // ALLOWS ENVIRONMENT VARIABLES TO BE SET ON PROCESS
 
 const express = require("express");
 
-
 // cors explanation https://www.youtube.com/watch?v=EdJ6RJ3uGbo
 const cors = require("cors");
 const app = express();
+
+const swaggerUi = require("swagger-ui-express"),
+  swaggerDocument = require("./swagger.json");
 
 // Middleware
 app.use(express.json()); // parse json bodies in the request object
@@ -13,6 +15,8 @@ app.use(cors()); // allow cross origin requests
 
 // Redirect requests to endpoint starting with /posts to postRoutes.js
 app.use("/api/students", require("./routes/StudentRoutes"));
+
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // Global Error Handler. IMPORTANT function params MUST start with err
 app.use((err, req, res, next) => {
